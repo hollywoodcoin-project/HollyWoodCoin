811e72c72801926ab49977fb75a2ddbd094d3abf
Mathieu Florentin <43298151+upperstate@users.noreply.github.com>
2019-04-15 20:58:55 +0000

    Emission ends since block 565916
    Further blocks reward contains transaction fees only.

--- src/main.cpp
+++ src/main.cpp
@@ -1018,6 +1018,9 @@ int64_t GetProofOfWorkReward(int64_t nFees)
 int64_t GetProofOfStakeReward(int64_t nCoinAge, int64_t nFees)
 {
     int64_t nSubsidy = nCoinAge * COIN_YEAR_REWARD * 33 / (365 * 33 + 8);
+    
+    if (pindexBest->nHeight >= 565916)
+        nSubsidy = 0;
 
     if (fDebug && GetBoolArg("-printcreation"))
         printf("GetProofOfStakeReward(): create=%s nCoinAge=%"PRId64"\n", FormatMoney(nSubsidy).c_str(), nCoinAge);
